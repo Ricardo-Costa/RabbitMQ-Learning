66e0a73ce256ded910183a1cd23cffcac97874e7
Ricardo Costa <ricardo18acs@gmail.com>
2021-12-30 17:38:42 -0300

    feat: basic comunication

--- server_a/src/app.js
+++ server_a/src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import amqp from "amqplib/callback_api.js"
 
 const app = express()
 
@@ -9,6 +10,39 @@ app.get('/', (req, res) => {
 app.post('/go-to-queue', (req, res) => {
   res.send('Ok!!')
 })
+
+amqp.connect('amqp://rabbit-mq:5672', (err, connection) => {
+  if (err) {
+    console.log(err)
+    throw err
+  }
+
+  connection.createChannel((err, channel) => {
+    if (err) {
+      console.log(err)
+      throw err
+    }
+
+    const queue = 'hello'
+    const msg = JSON.stringify({
+      msg: "Hello World 2022!!"
+    })
+
+    channel.assertQueue(queue, {
+      durable: false
+    })
+
+    channel.sendToQueue(queue, Buffer.from(msg))
+
+    console.log("MSG: %s", msg)
+
+    setTimeout(() => {
+      connection.close()
+      process.exit(0)
+    }, 500);
+
+  })
+});
 
 app.listen(3000, '0.0.0.0', () => {
   console.log('Server A okay...')
--- server_b/src/app.js
+++ server_b/src/app.js
@@ -1,6 +1,33 @@
 import express from "express"
+import amqp from "amqplib/callback_api.js"
 
 const app = express()
+
+amqp.connect('amqp://rabbit-mq:5672', (err, connection) => {
+  if (err) {
+    console.log(err)
+    throw err
+  }
+
+  connection.createChannel((err, channel) => {
+    if (err) {
+      console.log(err)
+      throw err
+    }
+
+    const queue = 'hello'
+
+    channel.assertQueue(queue, {
+      durable: false
+    })
+
+    channel.consume(queue, msg => {
+      console.log("Received %s", msg.content.toString());
+    }, {
+      noAck: true
+    })
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('Server B Okay!!')
